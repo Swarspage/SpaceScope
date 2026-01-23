820a421119b902b981be612efdadee9e5f66c107
Swar Shinde <162664467+Swarspage@users.noreply.github.com>
2026-01-24 04:31:13 +0530

    final4

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { AuthProvider, useAuth } from "../Context/AuthContext";
+import { AuthProvider, useAuth } from "./Context/AuthContext";
 import Dashboard from "./Pages/Dashboard";
 import LoginPage from "./Pages/LoginPage";
 import ProfilePage from "./Pages/ProfilePage";
